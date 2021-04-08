f17dbd1020609edb9c59740c585c7cc19d106445
Julio Lajara <ju2wheels@users.noreply.github.com>
2021-04-08 19:46:17 -0400

    Fix README formatting and bug in `--reset-chain` `join/joinTestnet.sh` argument. (#8)
    * Fix README formatting and bug in `--reset-chain` `join/joinTestnet.sh` argument.
    
    * Used 4 space indent on code blocks and text underneath numbered lists so it appears indented under each numbered item instead of looking like one left aligned runaway section.
    * `--reset-chain` argument for `join/joinTestnet.sh` is dangerous due to lack of proper shell variable quoting and allows for accidentally deleting far more data than just the Axelar root directory under the right circumstances. Directories with spaces in them would have removed two levels of directories, e.g. `rm -rf /first/absolute/path second/relative/path` vs `rm -rf "/one/path with/spaces/"`.
    
    * Update README to include dynamic gas flags just in case there are multiple transactions that need to be consolidated and clarify instructions on where to deposit tBTC from and where to withdraw to.

--- join/joinTestnet.sh
+++ join/joinTestnet.sh
@@ -6,10 +6,9 @@ AXELAR_CORE_VERSION=""
 TOFND_VERSION=""
 RESET_CHAIN=false
 ROOT_DIRECTORY=~/.axelar
-GIT_ROOT=$(git rev-parse --show-toplevel)
+GIT_ROOT="$(git rev-parse --show-toplevel)"
 
-for arg in "$@"
-do
+for arg in "$@"; do
   case $arg in
     --reset-chain)
     RESET_CHAIN=true
@@ -33,61 +32,58 @@ do
   esac
 done
 
-if [ -z "$AXELAR_CORE_VERSION" ]
-then
+if [ -z "$AXELAR_CORE_VERSION" ]; then
   echo "'--axelar-core vX.Y.Z' is required"
   exit 1
 fi
 
-if [ -z "$TOFND_VERSION" ]
-then
+if [ -z "$TOFND_VERSION" ]; then
   echo "'--tofnd vX.Y.Z' is required"
   exit 1
 fi
 
-if $RESET_CHAIN
-then
-  rm -rf $ROOT_DIRECTORY
+if $RESET_CHAIN; then
+  rm -rf "$ROOT_DIRECTORY"
 fi
 
-mkdir -p $ROOT_DIRECTORY
+mkdir -p "$ROOT_DIRECTORY"
 
-SHARED_DIRECTORY=$ROOT_DIRECTORY/shared
-mkdir -p $SHARED_DIRECTORY
+SHARED_DIRECTORY="${ROOT_DIRECTORY}/shared"
+mkdir -p "$SHARED_DIRECTORY"
 
-if [ ! -f $SHARED_DIRECTORY/genesis.json ]; then
-  curl https://axelar-testnet.s3.us-east-2.amazonaws.com/genesis.json -o $SHARED_DIRECTORY/genesis.json
+if [ ! -f "${SHARED_DIRECTORY}/genesis.json" ]; then
+  curl https://axelar-testnet.s3.us-east-2.amazonaws.com/genesis.json -o "${SHARED_DIRECTORY}/genesis.json"
 fi
 
-if [ ! -f $SHARED_DIRECTORY/peers.txt ]; then
-  curl https://axelar-testnet.s3.us-east-2.amazonaws.com/peers.txt -o $SHARED_DIRECTORY/peers.txt
+if [ ! -f "${SHARED_DIRECTORY}/peers.txt" ]; then
+  curl https://axelar-testnet.s3.us-east-2.amazonaws.com/peers.txt -o "${SHARED_DIRECTORY}/peers.txt"
 fi
 
-if [ ! -f $SHARED_DIRECTORY/config.toml ]; then
-  cp ${GIT_ROOT}/join/config.toml $SHARED_DIRECTORY/config.toml
+if [ ! -f "${SHARED_DIRECTORY}/config.toml" ]; then
+  cp "${GIT_ROOT}/join/config.toml" "${SHARED_DIRECTORY}/config.toml"
 fi
 
-if [ ! -f $SHARED_DIRECTORY/consumeGenesis.sh ]; then
-  cp ${GIT_ROOT}/join/consumeGenesis.sh $SHARED_DIRECTORY/consumeGenesis.sh
+if [ ! -f "${SHARED_DIRECTORY}/consumeGenesis.sh" ]; then
+  cp "${GIT_ROOT}/join/consumeGenesis.sh" "${SHARED_DIRECTORY}/consumeGenesis.sh"
 fi
 
-docker run \
-  --name tofnd \
-  -d \
+docker run       \
+  --name tofnd   \
+  -d             \
   -p 50051:50051 \
-  axelarnet/tofnd:$TOFND_VERSION
+  "axelarnet/tofnd:${TOFND_VERSION}"
 
-docker run \
-  --name axelar-core \
-  -p 1317:1317 \
-  -p 26656-26658:26656-26658 \
-  -p 26660:26660 \
-  --env TOFND_HOST=host.docker.internal \
-  --env START_REST=true \
-  --env PEERS_FILE=/root/shared/peers.txt \
-  --env INIT_SCRIPT=/root/shared/consumeGenesis.sh \
-  --env CONFIG_PATH=/root/shared/config.toml \
-  -v $ROOT_DIRECTORY/.axelard:/root/.axelard \
-  -v $ROOT_DIRECTORY/.axelarcli:/root/.axelarcli \
-  -v $SHARED_DIRECTORY:/root/shared \
-  axelarnet/axelar-core:$AXELAR_CORE_VERSION
+docker run                                           \
+  --name axelar-core                                 \
+  -p 1317:1317                                       \
+  -p 26656-26658:26656-26658                         \
+  -p 26660:26660                                     \
+  --env TOFND_HOST=host.docker.internal              \
+  --env START_REST=true                              \
+  --env PEERS_FILE=/root/shared/peers.txt            \
+  --env INIT_SCRIPT=/root/shared/consumeGenesis.sh   \
+  --env CONFIG_PATH=/root/shared/config.toml         \
+  -v "${ROOT_DIRECTORY}/.axelard:/root/.axelard"     \
+  -v "${ROOT_DIRECTORY}/.axelarcli:/root/.axelarcli" \
+  -v "${SHARED_DIRECTORY}:/root/shared"              \
+  "axelarnet/axelar-core:${AXELAR_CORE_VERSION}"
