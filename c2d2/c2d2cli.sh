d508cc80cc3182eaaa90b6d2ec6808713dac7a23
jack <jackson.virgo1@gmail.com>
2021-04-30 00:24:01 +0930

    build(c2d2): config setup script

--- c2d2/c2d2cli.sh
+++ c2d2/c2d2cli.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env bash
-
-docker run -it --entrypoint=""  -v "$HOME/.c2d2cli":/root/.c2d2cli --add-host=host.docker.internal:host-gateway axelar/c2d2cli:latest bash
+#!/bin/sh
+config_dir=${C2D2_HOME:-"$HOME/.c2d2cli"}
+SRC=$(git rev-parse --show-toplevel)
+sh ${src}/c2d2/install.sh
+docker run -it --entrypoint=""  -v "$config_dir":/root/.c2d2cli --add-host=host.docker.internal:host-gateway axelar/c2d2cli:latest bash
