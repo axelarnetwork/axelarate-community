2f92c01a3a4d84e92fbed3152310f31913b81317
jack <jackson.virgo1@gmail.com>
2021-05-10 17:41:58 +0930

    c2d2: pull latest image

--- c2d2/c2d2cli.sh
+++ c2d2/c2d2cli.sh
@@ -2,4 +2,5 @@
 config_dir=${C2D2_HOME:-"$HOME/.c2d2cli"}
 GIT_ROOT=$(git rev-parse --show-toplevel)
 sh "${GIT_ROOT}"/c2d2/config.sh
+docker pull axelarnet/c2d2:latest
 docker run -it --entrypoint=""  -v "${config_dir}":/root/.c2d2cli --add-host=host.docker.internal:host-gateway axelarnet/c2d2:latest bash
