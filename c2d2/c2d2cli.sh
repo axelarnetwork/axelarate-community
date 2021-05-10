fc86a8b2683ddf72fcf99188e49eaa5a315a8d38
jack <jackson.virgo1@gmail.com>
2021-05-10 13:51:38 +0930

    fix(c2d2): install.sh -> config.sh

--- c2d2/c2d2cli.sh
+++ c2d2/c2d2cli.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 config_dir=${C2D2_HOME:-"$HOME/.c2d2cli"}
 SRC=$(git rev-parse --show-toplevel)
-sh ${src}/c2d2/install.sh
+sh ${src}/c2d2/config.sh
 docker run -it --entrypoint=""  -v "$config_dir":/root/.c2d2cli --add-host=host.docker.internal:host-gateway axelarnet/c2d2:v0.1.0 bash
