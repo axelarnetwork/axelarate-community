b88f5230489fdae7513985e10ada906cd2107108
Haiyi Zhong <haiyizxx@gmail.com>
2021-06-07 18:37:26 -0400

    change from [[ to [

--- c2d2/config.sh
+++ c2d2/config.sh
@@ -6,12 +6,12 @@ config_dir=${C2D2_HOME:-"$HOME/.c2d2cli"}
 
 mkdir -p $config_dir
 
-if [[ -e ${config_dir}/config.toml ]]; then
+if [ -e "${config_dir}"/config.toml ]; then
   echo "Moving existing config.toml to ${config_dir}/config.old.toml"
   mv ${config_dir}/config.toml ${config_dir}/config.old.toml
 fi
 
-if [[ ! -e ${config_dir}/config.toml ]]; then
+if [ ! -e "${config_dir}"/config.toml ]; then
   echo "Copying config.toml to $config_dir"
   cp $config_file $config_dir/
 fi
