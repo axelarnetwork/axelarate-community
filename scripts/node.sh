930b5eedd20a8fe00140def582a8d0b605c65bea
kalidax <kalid@axelar.network>
2023-10-23 12:21:35 +0200

    fix: paths in scripts

--- scripts/node.sh
+++ scripts/node.sh
@@ -156,7 +156,7 @@ parse_params() {
   fi
 
   if [ -z "${axelar_core_version}" ]; then
-    axelar_core_version="$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelar-docs/main/pages/resources/"${network}".mdx  | grep axelar-core | cut -d \` -f 4)"
+    axelar_core_version="$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelar-docs/main/src/pages/resources/"${network}".mdx  | grep axelar-core | cut -d \` -f 4)"
   fi
 
   # check required params and arguments
