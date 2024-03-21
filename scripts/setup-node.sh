4e9faa0c032898fd090bf2d3e33cfc9dfb20e393
Stephen Fluin <stephen.fluin@gmail.com>
2024-03-20 22:37:47 -0500

    fix: correct hardcoded path to chain config in docs

--- scripts/setup-node.sh
+++ scripts/setup-node.sh
@@ -95,7 +95,7 @@ parse_params() {
     fi
 
     if [ -z "${axelar_core_version}" ]; then
-        axelar_core_version="$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelar-docs/main/src/pages/resources/"${network}".mdx | grep axelar-core | cut -d \` -f 4)"
+        axelar_core_version="$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelar-docs/main/src/pages/resources/contract-addresses/"${network}".mdx | grep axelar-core | cut -d \` -f 4)"
     fi
 
     # check required params and arguments
